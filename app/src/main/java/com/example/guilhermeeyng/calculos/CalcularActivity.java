c0656c1b08224d5ba0b1b380b248c39bbb1306c3
lutkemeyer <guilherme_eyng@hotmail.com>
2018-11-06 19:53:31 -0200

    conserto de bugs

--- app/src/main/java/com/example/guilhermeeyng/calculos/CalcularActivity.java
+++ app/src/main/java/com/example/guilhermeeyng/calculos/CalcularActivity.java
@@ -50,19 +50,13 @@ public class CalcularActivity extends AppCompatActivity {
 
             TextWatcher listenerDigitacao = new TextWatcher() {
                 @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
                     podeCalcular();
                 }
-
                 @Override
-                public void afterTextChanged(Editable s) {
-
-                }
+                public void afterTextChanged(Editable s) { }
             };
 
             for(int i=0; i < chaves.size(); i++){
